e7ed5f73abdb4bd651301ddde62952ce0241af94
Kreidl <lukaskienast0@gmail.com>
2020-05-24 07:42:11 +0200

    Revert "Added tmp api key"
    This reverts commit 9803ed89280fcfd42b16bd21837a8d301eb85a62.

--- src/main/java/com/kienast/jenkinstest/JenkinstestApplication.java
+++ src/main/java/com/kienast/jenkinstest/JenkinstestApplication.java
@@ -8,10 +8,6 @@ public class JenkinstestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinstestApplication.class, args);
-		
-		String api_key = "374d66dad8b2b10a54437d04e5fa83819a10b752";
-		
-		System.out.println(api_key);
 	}
 
 }
