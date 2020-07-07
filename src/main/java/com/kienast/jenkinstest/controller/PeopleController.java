d4cd66615eef9e832726c62c7f5a5de376e195aa
Lukas Kienast <lukas.kienast@nowconsultians.at>
2020-07-07 21:35:05 +0200

    Changed API Name

--- src/main/java/com/kienast/jenkinstest/controller/PeopleController.java
+++ src/main/java/com/kienast/jenkinstest/controller/PeopleController.java
@@ -1,36 +1,30 @@
 package com.kienast.jenkinstest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kienast.jenkinstest.dto.PeopleAdapter;
 import com.kienast.jenkinstest.dto.PersonAdapter;
 import com.kienast.jenkinstest.exception.PersonNotFoundException;
 import com.kienast.jenkinstest.model.Person;
-import com.kienast.jenkinstest.rest.api.GetPeopleApi;
+import com.kienast.jenkinstest.rest.api.PeopleApi;
 import com.kienast.jenkinstest.rest.api.model.PersonModel;
 import com.kienast.jenkinstest.service.PeopleService;
 
 
 @RestController
 @RequestMapping(value= "/api")
-public class PeopleController implements GetPeopleApi {
+public class PeopleController implements PeopleApi {
 	
 	@Autowired
 	private PeopleService peopleService;
 
 	@Override
-	@RequestMapping(value= "/getPeople/{personname}")
 	public ResponseEntity<PersonModel> getPerson(String personname) {
 		
 		try {
@@ -46,7 +40,6 @@ public class PeopleController implements GetPeopleApi {
 	}
 
 	@Override
-	@RequestMapping(value= "/getPeople")
 	public ResponseEntity<List<PersonModel>> getPeople() {
 		List<Person> people = peopleService.getPeople();	
 		List<PersonModel> response = people.stream().map(PeopleAdapter::new)
--- src/main/java/com/kienast/jenkinstest/rest/api/PeopleApi.java
+++ src/main/java/com/kienast/jenkinstest/rest/api/PeopleApi.java
@@ -10,36 +10,28 @@ import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 import java.util.List;
-import java.util.Map;
 
 @Validated
-@Api(value = "getPeople", description = "the getPeople API")
-public interface GetPeopleApi {
+@Api(value = "people", description = "the people API")
+public interface PeopleApi {
 
-    @ApiOperation(value = "GetPeople", nickname = "getPeople", notes = "", response = PersonModel.class, responseContainer = "List", tags={ "getPeople", })
+    @ApiOperation(value = "GetPeople", nickname = "getPeople", notes = "", response = PersonModel.class, responseContainer = "List", tags={ "people", })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Schemas", response = PersonModel.class, responseContainer = "List") })
-    @RequestMapping(value = "/getPeople",
+    @RequestMapping(value = "/people",
         produces = { "application/json" }, 
         method = RequestMethod.GET)
     ResponseEntity<List<PersonModel>> getPeople();
 
 
-    @ApiOperation(value = "Get Person", nickname = "getPerson", notes = "", response = PersonModel.class, tags={ "getPerson", })
+    @ApiOperation(value = "Get Person", nickname = "getPerson", notes = "", response = PersonModel.class, tags={ "people", })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Schemas", response = PersonModel.class) })
-    @RequestMapping(value = "/getPeople/{personname}",
+    @RequestMapping(value = "/people/{personname}",
         produces = { "application/json" }, 
         method = RequestMethod.GET)
     ResponseEntity<PersonModel> getPerson(@ApiParam(value = "a Name of a Person",required=true) @PathVariable("personname") String personname);
